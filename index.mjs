518ad82bbfc9fb03cdfef57a74d8da2f8a3b9dec
Scott Cooper <scttcper@gmail.com>
2024-09-22 21:49:52 -0700

    fix: remove rule

--- index.mjs
+++ index.mjs
@@ -107,21 +107,6 @@ const tsEslintConfig = tseslint.config(
 
       '@typescript-eslint/no-non-null-asserted-nullish-coalescing': ['error'],
       '@typescript-eslint/no-non-null-asserted-optional-chain': ['error'],
-      '@typescript-eslint/no-restricted-imports': [
-        'error',
-        {
-          paths: [
-            'error',
-            'domain',
-            'freelist',
-            'smalloc',
-            'punycode',
-            'sys',
-            'querystring',
-            'colors',
-          ],
-        },
-      ],
 
       '@typescript-eslint/no-require-imports': ['error'],
       '@typescript-eslint/only-throw-error': [
